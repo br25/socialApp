62145440c55ae7acd0a2525289a50c1bf80e24e5
br25 <md.ashrafulislamrobin12345@gmail.com>
2022-10-29 07:50:03 +0600

    post detail create

--- post_feed/urls.py
+++ post_feed/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import PostList
+from .views import PostList, PostDetail
 
 urlpatterns = [
     path('posts', PostList.as_view(), name='posts'),
+	path('posts/<int:pk>', PostDetail.as_view(), name='post-details'),
 ]
--- post_feed/views.py
+++ post_feed/views.py
@@ -1,10 +1,14 @@
 from rest_framework import status
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 from .models import Post, Comment, Like
 from .serializers import PostSerializer, CommentSerializer, LikeSerializer
 
 
 class PostList(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
