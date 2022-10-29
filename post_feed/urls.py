cdd045cfea74b5435366c8d3df3f1f41586bfb0e
br25 <md.ashrafulislamrobin12345@gmail.com>
2022-10-29 07:26:33 +0600

    gitignore check

--- post_feed/urls.py
+++ post_feed/urls.py
@@ -6,6 +6,4 @@ from .views import PostListView
 
 urlpatterns=[
 	path('', PostListView.as_view(), name='home'),
-	# path('post/new/', views.create_post, name='post-create'),
-	# path('post/<int:pk>/', views.post_detail, name='post-detail')
 ]
